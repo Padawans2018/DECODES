dbd6c034dca3dd879372970838cd45a36cbb9943
HennequinLaura <33421605+Laura369@users.noreply.github.com>
2018-04-06 12:36:30 +0200

    Read XML class

--- DECODES_Laura/src/jenaTest/ReadXMLFile.java
+++ DECODES_Laura/src/jenaTest/ReadXMLFile.java
@@ -1,22 +1,18 @@
 package jenaTest;
 
-import xml.javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
 import java.io.File;
-
 
 
 public class ReadXMLFile {
 
-  public static void main(String argv[]) {
+  public static void main(String[] args) {
 
     try {
 
-	File fXmlFile = new File("C:\\Users\\laura\\OneDrive\\Documents\\Centrale\\S8\\DECODES");
+	File fXmlFile = new File("C:\\Users\\laura\\OneDrive\\Documents\\Centrale\\S8\\DECODES\\stEtienne\\example.xml");
 	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 	Document doc = dBuilder.parse(fXmlFile);
@@ -24,29 +20,6 @@ public class ReadXMLFile {
 	doc.getDocumentElement().normalize();
 
 	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-			
-	NodeList nList = doc.getElementsByTagName("staff");
-			
-	System.out.println("----------------------------");
-
-	for (int temp = 0; temp < nList.getLength(); temp++) {
-
-		Node nNode = nList.item(temp);
-				
-		System.out.println("\nCurrent Element :" + nNode.getNodeName());
-				
-		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-
-			Element eElement = (Element) nNode;
-
-			System.out.println("Staff id : " + eElement.getAttribute("id"));
-			System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
-			System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
-			System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
-			System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
-
-		}
-	}
     } catch (Exception e) {
 	e.printStackTrace();
     }
